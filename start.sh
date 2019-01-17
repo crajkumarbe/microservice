b4027b4d0078f576dbc245269d7d171b213fbe20
rpolu <rpolu@10.104.68.98>
2019-01-17 19:28:03 +0530

    added the new files to deploy

--- start.sh
+++ start.sh
@@ -15,25 +15,25 @@ clear
 
 echo Discovery Service: BUILD DONE
 echo API Gateway: BUILD DONE
-echo Autoattendent Service: BUILDING
-cd autoattendent-service
+echo Customer Service: BUILDING
+cd customer-service
 mvn clean install -U
 cd ..
 clear
 
 echo Discovery Service: BUILD DONE
 echo API Gateway: BUILD DONE
-echo Huntgroup Service: BUILD DONE
-echo Huntgroup Service: BUILDING
-cd huntgroup-service
+echo Customer Service: BUILD DONE
+echo Order Service: BUILDING
+cd order-service
 mvn clean install -U
 cd ..
 clear
 
 echo Discovery Service: BUILD DONE
 echo API Gateway: BUILD DONE
-echo Autoattendent Service: BUILD DONE
-echo Huntgroup Service: BUILD DONE
+echo Customer Service: BUILD DONE
+echo Order Service: BUILD DONE
 echo ---
 echo Starting Application...
 docker-compose up --build
